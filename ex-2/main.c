eee8b00dbd650eef2efdcd60b7f66ac554ad5b61
MSlezak2 <michalsleezak@gmail.com>
2022-05-28 11:21:21 +0200

    Add area calculation feature
    Apart from the perimeter, now the area of figures is also being displayed.

--- ex-2/main.c
+++ ex-2/main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define LENGTH_OF(x) sizeof(x)/sizeof(x[0])
 #define PI 3.141592
+#define CIRCLE_PERIMETER(r) 2*PI*r
+#define CIRCLE_AREA(r) PI*r*r
+#define RECTANGLE_PERIMETER(a,b) 2*a+2*b
+#define RECTANGLE_AREA(a,b) a*b
+#define TRIANGLE_PERIMETER(a,b,c) a+b+c
+#define TRIANGLE_AREA(a,b,c,s) sqrt( s*(s-a)*(s-b)*(s-c) ) //Heron's formula; s-perimeter devided by 2
+#define TRAPEZOID_PERIMETER(a,b,c,d) a+b+c+d
+//#define TRAPEZOID_AREA(a,b,c,d) 
 
 float take_float_from_user(char* name);
 char let_user_decide(char* possibilities_keys[], char possibilities_values[], int no_possibilities);
@@ -12,7 +21,8 @@ int is_choice_valid(char possibilities[], int no_posssibilities, char users_choi
 int main() 
 {
 	char users_choice = '0';
-	float radius, a, b, c, d;
+	float r, a, b, c, d, h;
+	float perimeter, area;
 
 	do
 	{
@@ -21,7 +31,7 @@ int main()
 		case '0': // state START
 			system("cls");
 			printf("Welcome to our super duper geometric calculator. Tell me, what figure do you have in mind?\n\n");
-			char* possibilities_keys[] = { "CIRCLE","RECTANGLE","TRIANGLE","TRAPEZE","Just get me outta here" };
+			char* possibilities_keys[] = { "CIRCLE","RECTANGLE","TRIANGLE","TRAPEZOID","Just get me outta here" };
 			char possibilities_values[] = { '1','2','3','4','x' };
 			int no_possibilities = LENGTH_OF(possibilities_keys);
 			users_choice = let_user_decide(possibilities_keys, possibilities_values, no_possibilities);
@@ -33,10 +43,15 @@ int main()
 			system("cls");
 			printf("You've chosen a circle. Now please enter the value of the radius...\n\n");
 			
-			radius = take_float_from_user("radius");
+			r = take_float_from_user("r");
+			perimeter = CIRCLE_PERIMETER(r);
+			area = CIRCLE_AREA(r);
 			
 			system("cls");
-			printf("The perimeter of your figure is: %f", 2*PI*radius);
+			printf("r = %f\n", r);
+			printf("The perimeter of your figure is: %f\n", perimeter);
+			printf("The area of your figure: %f\n\n", area);
+			printf("Press Any Key to Continue\n"); // to give the user time to read
 			getch();
 
 			users_choice = '0'; //return to the START state
@@ -51,8 +66,14 @@ int main()
 			a = take_float_from_user("a");
 			b = take_float_from_user("b");
 
+			perimeter = RECTANGLE_PERIMETER(a, b);
+			area = RECTANGLE_AREA(a, b);
+
 			system("cls");
-			printf("The perimeter of your figure is: %f", 2*a+2*b);
+			printf("a = %f\tb = %f\n", a, b);
+			printf("The perimeter of your figure is: %f\n", perimeter);
+			printf("The area of your figure: %f\n\n", area);
+			printf("Press Any Key to Continue\n"); // to give the user time to read
 			getch();
 
 			users_choice = '0'; //return to the START state
@@ -68,8 +89,14 @@ int main()
 			b = take_float_from_user("b");
 			c = take_float_from_user("c");
 
+			perimeter = TRIANGLE_PERIMETER(a, b, c);
+			area = TRIANGLE_AREA(a, b, c, perimeter / 2);
+
 			system("cls");
-			printf("The perimeter of your figure is: %f", a+b+c);
+			printf("a = %f\tb = %f\tc = %f\n", a, b, c);
+			printf("The perimeter of your figure is: %f\n", perimeter);
+			printf("The area of your figure: %f\n\n", area);
+			printf("Press Any Key to Continue\n"); // to give the user time to read
 			getch();
 
 			users_choice = '0'; //return to the START state
@@ -77,7 +104,7 @@ int main()
 			break;
 
 
-		case '4': // state TRAPEZE
+		case '4': // state TRAPEZOID
 			system("cls");
 			printf("You've chosen a triangle. Now please enter the length of the consecutive sides...\n\n");
 
@@ -85,9 +112,16 @@ int main()
 			b = take_float_from_user("b");
 			c = take_float_from_user("c");
 			d = take_float_from_user("d");
+			h = take_float_from_user("h");
+
+			perimeter = TRAPEZOID_PERIMETER(a, b, c, d);
+			area = ((a + b) / 2) * h;
 
 			system("cls");
-			printf("The perimeter of your figure is: %f", a + b + c + d);
+			printf("a = %f\tb = %f\tc = %f\td = %f\th = %f\n", a,b,c,d,h);
+			printf("The perimeter of your figure is: %f\n", perimeter);
+			printf("The perimeter of your figure is: %f\n\n", area);
+			printf("Press Any Key to Continue\n"); // to give the user time to read
 			getch();
 
 			users_choice = '0'; //return to the START state
@@ -111,7 +145,8 @@ float take_float_from_user(char* name)
 		if (number == 0)
 		{
 			system("cls");
-			printf("Make sure you enter correct value...");
+			printf("Make sure you enter correct value...\n\n");
+			printf("Press Any Key to Continue\n"); // to give the user time to read
 			getch();
 		}
 	} while (number == 0);
@@ -161,6 +196,7 @@ int is_choice_valid(char possibilities[], int no_posssibilities, char users_choi
 	return is_valid;
 }
 
+// TODO: Find formula for the area of trapezoid
 // TODO: Test various cases of input, whether the output is correct? (long input, out-of-range input, etc.)
 // TODO: Restrict the value of sides (prevent overflow)
 // TODO: Make sure that user enters correct length of sides (is it possible to make, say a rectangle out of them?)
