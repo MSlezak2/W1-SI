3f95222ba5b082c2061e64c1ca23ce48b2daae4c
MSlezak2 <michalsleezak@gmail.com>
2022-05-29 20:56:27 +0200

    Add menu and stats features
    User can choose in menu whether he wants to display grades of some particular
    student or to take a look at statistics. He can also add a new student to the
    'database'.

--- ex-3/main.c
+++ ex-3/main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <string.h>
 
 #define LENGTH_OF(x) sizeof(x)/sizeof(x[0])
@@ -19,71 +19,166 @@ int is_eligible(float grades[], int no_grades);
 
 int main()
 {
-	/*float harper_eland[7] = { 56.7, 22, 60, 45, 96, 19.5, 59.7 };
-	float celement_hayes[7] = { 40.2, 80, 57, 49.1, 38.5, 89.7, 96.1};
-	float belinsa_jacobs[7] = { 90.2, 79.3, 95.9, 95.1, 97.1, 79.4, 80.1};
-	float cedric_santos[7] = { 78.2, 50.1, 58.63, 80.2, 18.4, 96.3, 96.1 };
-	float fiona_wilcher[7] = { 95.2, 99.2, 82.4, 60.77, 90.24, 97.56, 90.1};
-	*/
+	// populate database
+	students[0] = "Michal";
+	scores[0][0] = 96.0;
+	scores[0][1] = 96.0;
+	scores[0][2] = 96.0;
+	scores[0][3] = 91.0;
+	scores[0][4] = 91.0;
+	scores[0][5] = 91.0;
+	scores[0][6] = 91.0;
+	grades[0][0] = 'A';
+	grades[0][1] = 'A';
+	grades[0][2] = 'A';
+	grades[0][3] = 'A';
+	grades[0][4] = 'A';
+	grades[0][5] = 'A';
+	grades[0][6] = 'A';
+	students[1] = "Maciek";
+	scores[1][0] = 50.0;
+	scores[1][1] = 50.0;
+	scores[1][2] = 96.0;
+	scores[1][3] = 91.0;
+	scores[1][4] = 91.0;
+	scores[1][5] = 91.0;
+	scores[1][6] = 91.0;
+	grades[1][0] = 'F';
+	grades[1][1] = 'F';
+	grades[1][2] = 'A';
+	grades[1][3] = 'A';
+	grades[1][4] = 'A';
+	grades[1][5] = 'A';
+	grades[1][6] = 'A';
+	no_students+=2;
 
-	//students[0] = "Michal";
-	//scores[0][0] = 96.0;
-	//scores[0][1] = 96.0;
-	//scores[0][2] = 91.0;
-	//scores[0][3] = 91.0;
-	//scores[0][4] = 91.0;
-	//scores[0][5] = 91.0;
-	//scores[0][6] = 91.0;
-	//grades[0][0] = 'A';
-	//grades[0][1] = 'A';
-	//grades[0][2] = 'A';
-	//grades[0][3] = 'A';
-	//grades[0][4] = 'A';
-	//grades[0][5] = 'A';
-	//grades[0][6] = 'A';
-
-	//Enter students details
-
-	system("cls");
-	printf("TEACHER'S ASSISTANT");
-	printf("\n\nEnter the name of the student:\t");
-	scanf_s("%[^\n]s", &students[no_students], 30);
-
-	printf("\n\nNow enter consecutive scores, that student has obtained throughout the semester in following subjects:\t");
-
-	for (int i = 0; i < LENGTH_OF(subjects); i++)
+	char users_choice[30] = "";
+	do
 	{
-		scores_buffer = 0;
-		printf("\n\n%s:",subjects[i]);
-		for (int j = 0; j < NUMBER_OF_NOTES_IN_SUBJECT; j++)
+		// MAIN MENU
+		system("cls");
+
+		// Show user what options does he have
+		printf("TEACHER'S ASSISTANT");
+		printf("\n\nChoose one of the following options:\n\n");
+		for (int i = 0; i < no_students; i++)
 		{
-			printf("\n\n\t%i:\t", j+1);
-			scanf_s("%f", &temp_score);
-			scores_buffer += temp_score;
+			printf("%i - %s\n", i+1, students[i]);
 		}
-		scores[no_students][i] = scores_buffer / NUMBER_OF_NOTES_IN_SUBJECT; // average score in subject
-		grades[no_students][i] = give_grade(scores[no_students][i]); // grade for subject
-	}
-	no_students++;
+		int add_option_number = no_students + 1;
+		int stats_option_number = no_students + 2;
+		if (no_students < 8) printf("%i - ADD\n", add_option_number);
+		printf("%i - STATS\n", stats_option_number);
+		printf("x - EXIT\n");
 
-	// Print  received grades
+		// Take user's input and go to the appropriate conditional block of instruction
+		scanf_s("%s", &users_choice, 30);
 
-	for (int i = 0; i < LENGTH_OF(subjects); i++)
-	{
-		printf("\n\n%s:\t%c", subjects[i], grades[0][i]);
-	}
+		int chosen_number = atoi(users_choice); // id of chosen student (if input was not an int, it equals to 0) 
 
-	if (is_eligible(scores[0],7))
-	{
-		printf("\n\nStudent is eligible for scholarship\n\n");
-	}
-	else
-	{
-		printf("\n\nStudent is not eligible for scholarship\n\n");
-	}
+		// Block for displaying students' details
+		if (chosen_number > 0 && chosen_number <= no_students) // if user chose one of the students
+		{
+			// Print  received grades
+			system("cls");
+			for (int i = 0; i < LENGTH_OF(subjects); i++)
+			{
+				printf("\n\n%s:\t%.2f%%\t(%c)", subjects[i], scores[chosen_number-1][i], grades[chosen_number - 1][i]);
+			}
+
+			if (is_eligible(scores[chosen_number - 1],7))
+			{
+				printf("\n\nStudent is eligible for scholarship\n\n");
+			}
+			else
+			{
+				printf("\n\nStudent is not eligible for scholarship\n\n");
+			}
+			printf("Press any key to return...");
+			getch();
+		}
+
+		// Handle adding new students to data base
+		if (chosen_number == add_option_number && no_students < 8) // ADD state
+		{
+			system("cls");
+			char temp[30] = "";
+			printf("\n\nEnter the name of the student:\t");
+			scanf_s("\n%[^\n]s", &temp, 30);
+			students[no_students] = temp;
+
+			printf("\n\nNow enter consecutive scores, that student has obtained throughout the semester in following subjects:\t");
+
+			for (int i = 0; i < LENGTH_OF(subjects); i++)
+			{
+				scores_buffer = 0;
+				printf("\n\n%s:",subjects[i]);
+				for (int j = 0; j < NUMBER_OF_NOTES_IN_SUBJECT; j++)
+				{
+					printf("\n\n\t%i:\t", j+1);
+					scanf_s("%f", &temp_score);
+					scores_buffer += temp_score;
+				}
+				// giving grades based on the average score
+				scores[no_students][i] = scores_buffer / NUMBER_OF_NOTES_IN_SUBJECT; // average score in subject
+				grades[no_students][i] = give_grade(scores[no_students][i]); // grade for subject
+			}
+			no_students++;
+		}
+
+		// Calculating and displaying stats regarding each subject and the sholarship
+		if (chosen_number == stats_option_number) // STATS state
+		{
+			system("cls");
+			float average_score, min_score, max_score, current_score;
+			int no_eligible_students = 0;
+
+			printf("Statistics for each subject:");
+			for (int i = 0; i < LENGTH_OF(subjects); i++) // subjects' loop
+			{
+				min_score = _CRT_INT_MAX;
+				max_score = 0;
+				scores_buffer = 0;
+				for (int j = 0; j < no_students; j++) //students' loop
+				{
+					current_score = scores[j][i];
+					scores_buffer += current_score;
+					// the highest note
+					if (current_score > max_score)
+					{
+						max_score = current_score;
+					}
+					// the lowest note
+					if (current_score < min_score)
+					{
+						min_score = current_score;
+					}
+				}
+				// average class note in each subject
+				average_score = scores_buffer / (float)no_students;
+
+				printf("\n\n%s:\tmin:%.2f%%\tmax:%.2f%%\tavg:%.2f%%", subjects[i], min_score, max_score, average_score);
+			}
+			
+			// number of students with scholarship
+			for (int i = 0; i < no_students; i++) //students' loop
+			{
+				no_eligible_students += is_eligible(scores[i], 7);
+			}
+
+
+			printf("\n\nNumber of students eligible for scholarship:\t%i", no_eligible_students);
+			printf("\n\nPress any key to return...");
+			getch();
+		}
+
+
+	} while (strcmp(&users_choice, "x"));
 
 	return 0;
 }
+
+
 
 char give_grade(float score_percentage)
 {
