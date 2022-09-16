6007a73536e47701c26894d841dc50cf571a7f74
MSlezak2 <michalsleezak@gmail.com>
2022-09-16 23:57:43 +0200

    Refactor first phase of the program
    To keep the main() function concise in matter of the level of abstraction
    rest of the file should also be refactored.

--- ex-5-worldle/main.c
+++ ex-5-worldle/main.c
@@ -13,40 +13,25 @@ const char* YELLOW = "[0;33m";
 const short int NO_ATTEMPTS = 7; 
 const short int MAX_NO_LETTERS = 10;
 
+char* make_up_word();
+char* ask_for_the_word();
+void validate_word(char* the_word);
+
 void read_word_incognito(char* string, int string_max_size);
 int contains_only_letters(char* s);
-void print_asterisk(int how_many);
+int is_in_word(char letter, char* word);
+void print_asterisks(int how_many);
 void print_in_color(char letter, char* color_code);
-int does_belong_to_the_word(char letter, char* word);
 void print_colored_word(char* the_word, char* the_guess, int word_size);
 //void discard_input_buffer();
 
 int main() {
 
 // MAKE UP A WORD PHASE
-	printf("Please enter a word (max %i letters)\n", MAX_NO_LETTERS);
-
-	// read the word 
-	char* the_word = malloc(sizeof(char)*(MAX_NO_LETTERS + 1)); // char the_word[WORD_SIZE] would be more effective
-	printf("\n");
-	read_word_incognito(the_word, MAX_NO_LETTERS + 1);
-
-	// validate it(should be invisible)
-	//TODO: ------------------------------------- extract to function --------------------------
-	while (!contains_only_letters(the_word) || strlen(the_word) == 0) {  // if entered word is too long, scanf_s() doesn't return anything
-		system("cls");
-		if (strlen(the_word) == 0) {
-			printf("Given word is too long (max length: %i). Let's try again...\n\n", MAX_NO_LETTERS);
-		} else if (!contains_only_letters(the_word)) {
-			printf("Given word contains forbidden characters. Let's try again...\n\n");
-		}
-		//discard_input_buffer();
-		read_word_incognito(the_word, MAX_NO_LETTERS + 1);
-	}
-	//-------------------------------------------------------------------------------------------
+	char* the_word = make_up_word();
 
 // GUESS THE WORD PHASE
-
+	
 	char* the_guess = malloc(sizeof(char) * (MAX_NO_LETTERS + 1));
 
 	int attempt = 1;
@@ -54,7 +39,7 @@ int main() {
 		// display a number of letters (***)
 		system("cls");
 		printf("The word: ");
-		print_asterisk(strlen(the_word));
+		print_asterisks(strlen(the_word));
 		printf("\n\n");
 
 		// keep asking until he gets that right
@@ -102,6 +87,40 @@ int main() {
 	return 0;
 }
 
+
+
+
+char* make_up_word() {
+	char* the_word = ask_for_the_word();
+	validate_word(the_word);
+	
+	return the_word;
+}
+
+char* ask_for_the_word() {
+	printf("Please enter a word (max %i letters)\n", MAX_NO_LETTERS);
+	char* the_word = malloc(sizeof(char) * (MAX_NO_LETTERS + 1)); // char the_word[WORD_SIZE] would probably be more effective
+	printf("\n");
+
+	read_word_incognito(the_word, MAX_NO_LETTERS + 1);
+
+	return the_word;
+}
+
+void validate_word(char* the_word) {
+	while (!contains_only_letters(the_word) || strlen(the_word) == 0) {  // if entered word is too long, scanf_s() doesn't return anything
+		system("cls");
+		if (strlen(the_word) == 0) {
+			printf("Given word is too long (max length: %i). Let's try again...\n\n", MAX_NO_LETTERS);
+		} else if (!contains_only_letters(the_word)) {
+			printf("Given word contains forbidden characters. Let's try again...\n\n");
+		}
+		//discard_input_buffer();
+		read_word_incognito(the_word, MAX_NO_LETTERS + 1);
+	}
+}
+
+
 void read_word_incognito(char* string, int string_max_size) {
 	// returns empty string if word exceeds string_max_size
 	char c;
@@ -135,7 +154,13 @@ int contains_only_letters(char* s) {
 	return contains_only_letters;
 }
 
-void print_asterisk(int how_many) {
+int is_in_word(char letter, char* word) {
+	char* occurence = strchr(word, letter);
+
+	return occurence != NULL;
+}
+
+void print_asterisks(int how_many) {
 	for (int i = 0; i < how_many; i++) {
 		putchar('*');
 	}
@@ -149,18 +174,12 @@ void print_in_color(char letter, char* color_code) {
 	printf("\033[0m"); //Resets the text to default color
 }
 
-int does_belong_to_the_word(char letter, char* word) {
-	char* occurence = strchr(word, letter);
-
-	return occurence != NULL;
-}
-
 void print_colored_word(char* the_word, char* the_guess, int word_size) {
 	
 	for (int i = 0; i < word_size; i++) {
 		if (the_word[i] == the_guess[i]) {
 			print_in_color(the_guess[i], GREEN);
-		} else if (does_belong_to_the_word(the_guess[i],the_word)) {
+		} else if (is_in_word(the_guess[i],the_word)) {
 			print_in_color(the_guess[i], YELLOW);
 		} else {
 			putchar(the_guess[i]);
